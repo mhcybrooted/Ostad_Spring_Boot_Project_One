99de12adb883b9ba583669b6c062e0bacbf5caab
mh-dpi <honey.app.developer@gmail.com>
2025-07-30 21:01:58 +0600

    Initial commit

---
+++ src/main/java/mh/cyb/root/Main.java
@@ -0,0 +1,18 @@
+package mh.cyb.root;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your Full Name : ");
+        String fullName = scanner.nextLine();
+
+        System.out.println("Name : "+fullName);
+        System.out.println("why learning this course");
+        System.out.println("what is your carrier goal");
+    }
+
+}
